b1165d7dbc8d001024254d8d26bc740022f4d2a8
Jonas Bernoulli <jonas@bernoul.li>
2017-08-19 17:44:00 +0200

    [PATCH] Add library flim.el whose name matches that of the package flim
    A package should always contain a library with the same name.  One
    benefit of that is that Melpa can then extract the package description
    from the library commentary.
    
    Remove "flim-pkg.el".  The only additional information it contains is a
    version string, which has not ever been bumped since it was first added
    in 2014.
    
    [Emacsmirror] This commit is being rebased onto upstream.

--- flim-pkg.el
+++
@@ -1,6 +0,0 @@
-(define-package "flim" "1.14.9"
-  "A library to provide basic features about message representation or encoding."
-  '((emacs "24.5")
-    (apel "10.8")
-    (oauth2 "0.11"))
-  :maintainers '(("Kazuhiro Ito" . "kzhr@d1.dion.ne.jp")))
