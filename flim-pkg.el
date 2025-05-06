3c33170e82433486b941ffba8fdcd5c1e957c112
Kazuhiro Ito <kzhr@d1.dion.ne.jp>
2025-05-06 14:03:30 +0900

    * flim-pkg.el: Add :maintainers property.

--- flim-pkg.el
+++ flim-pkg.el
@@ -2,4 +2,5 @@
   "A library to provide basic features about message representation or encoding."
   '((emacs "24.5")
     (apel "10.8")
-    (oauth2 "0.11")))
+    (oauth2 "0.11"))
+  :maintainers '(("Kazuhiro Ito" . "kzhr@d1.dion.ne.jp")))
